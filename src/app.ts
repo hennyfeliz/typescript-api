8a73d6688f702d5ef5448837958daa81b5b9cffe
Henny M. Feliz Ferreras <mingito.2017@gmail.com>
2023-01-17 20:00:26 -0600

    Changes

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import router from "./routes/Item";
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+app.use(cors());
+
+app.use(router);
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
